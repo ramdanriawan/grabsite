39a47efda4a9eaa2c1568565982144a5302acddd
ramdan <ramdanriawan3@gmail.com>
2017-09-11 11:59:34 +0700

    initial project

---
+++ artikel.php
@@ -0,0 +1,122 @@
+<?php include "header.php"; ?>
+
+<main class="main col-md-7">
+    <content class="content">
+      <!-- Judul Berita Disini -->
+      <div class="row">
+        <div class="col-md-12 text-center text-capitalize">
+          <h2 class="data">Fasilitas Olahraga Pertama di Taman BMW Mulai Dibangun</h2>
+        </div>
+      </div>
+
+      <!--  Gambar Berita -->
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+          <div class="thumbnail">
+            <!--  gambar berita-->
+            <img class="" src="gambar/ahok.jpg" alt="">
+
+            <!-- caption gambar berita -->
+            <div class="caption">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--  data tanggal dan tag -->
+      <div class="row data_tanggal">
+        <div class="col-md-12 data text-capitalize">
+          <span><b>25 january 2017</b></span>
+          <span><b><a href="#"> | Otomotif</a></b></span>
+        </div>
+      </div>
+
+      <!--  hasil berita -->
+      <div class="row">
+        <div class="col-md-12">
+          <article class="article">
+            <strong>JAKARTA, KOMPAS.com</strong> - Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </article>
+        </div>
+      </div>
+
+      <!-- mencamtumkan link sumber -->
+      <div class="row sumber">
+        <div class="col-md-12">
+          <span class="sumber_link">Link Sumber: <b><a href="#">Kompas.com</a></b></span>
+        </div>
+      </div>
+    </content>
+
+<!-- pembungkus -->
+<div class="bungkus">
+
+<!--  list related berita akan tampil disini -->
+    <div class="related">
+      <ul>
+        <li class="related_header">Berita Terkait</li>
+        <li>
+          <div class="judul">
+            <a href="#beritaterpopuler1">berita terpopuler ke 1  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a>
+          </div>
+        </li>
+        <li>
+          <div class="judul">
+            <a href="#beritaterpopuler1">berita terpopuler ke 1  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a>
+          </div>
+        </li>
+        <li>
+          <div class="judul">
+            <a href="#beritaterpopuler1">berita terpopuler ke 1  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a>
+          </div>
+        </li>
+        <li>
+          <div class="judul">
+            <a href="#beritaterpopuler1">berita terpopuler ke 1  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+
+  <!-- berita lainnya -->
+      <div class="lainnya">
+        <ul>
+          <li class="related_header">Berita Lainnya</li>
+          <li>
+            <div class="judul">
+              <a href="#beritaterpopuler1">berita terpopuler ke 1  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a>
+            </div>
+          </li>
+          <li>
+            <div class="judul">
+              <a href="#beritaterpopuler1">berita terpopuler ke 1  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a>
+            </div>
+          </li>
+          <li>
+            <div class="judul">
+              <a href="#beritaterpopuler1">berita terpopuler ke 1  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a>
+            </div>
+          </li>
+          <li>
+            <div class="judul">
+              <a href="#beritaterpopuler1">berita terpopuler ke 1  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+</main>
+
+<?php include "footer.php"; ?>
---
+++ footer.php
@@ -0,0 +1,31 @@
+    <footer class="footer">
+      <!--  menu bagian kiri -->
+      <div class="row">
+        <div class="col-md-6">
+          <ul class="nav nav-tabs nav-justified">
+            <li><a class="active" href="about.php">About Us</a></li>
+            <li><a href="#menu2">Privacy Policy</a></li>
+            <li><a href="#menu3">Contact Us</a></li>
+            <li><a href="#menu4">Sitemap</a></li>
+          </ul>
+        </div>
+
+      <!--  menu bagian kanan -->
+        <div class="col-md-6">
+          <ul class="nav nav-tabs pull-right">
+            <li><a href="http://fb.com/ramdan.riawan2">Facebook</a></li>
+            <li><a href="http://instagram.com/ramdanriawan">Instagram</a></li>
+            <li><a href="http://line.com">Line</a></li>
+            <li><a href="http://api.whatsapp.com">WhatsApp</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="text-uppercase text-center">
+          &copy;My Site <?php echo date("Y" );?>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
